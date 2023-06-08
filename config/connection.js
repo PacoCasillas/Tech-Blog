cd84a93b71c6beb38f5df460be79f4dbbaa0e0d2
PacoCasillas <pacocasillasmx@gmail.com>
2023-06-08 13:28:27 -0400

    Added CMS blog site functionality

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,16 @@
-const Sequelize = require('sequelize');
-require('dotenv').config();
+const Sequelize = require("sequelize");
+require("dotenv").config();
 
-let sequelize;
-
-
+// Create a new Sequelize instance with the databse credentials from the .env file
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: "localhost",
+    port: 3306,
+    dialect: "mysql",
+  }
+);
 
 module.exports = sequelize;
