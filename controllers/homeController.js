113b8cab5a0a232cf70853e421d31e00cbee4b30
PacoCasillas <pacocasillasmx@gmail.com>
2023-06-09 12:23:00 -0400

    edited server.js and completed homeController

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,7 +1,31 @@
+const express = require('express');
 const router = require('express').Router();
-const { Project, User } = require('../models');
-const withAuth = require('../utils/auth');
+const { BlogPost, User } = require('../models');
 
+// const withAuth = require('../utils/auth');
 
+// Home route
+router.get('/', async (req, res) => {
+    try {
+        // Fetch all existing blog posts from the database
+        const blogPost = await BlogPost.findAll({
+            include: [
+                {
+                    model: User,
+                    attributes: ['username'],
+                },
+            ],
+        });
+
+        // Determine if the user is logged in
+        const loggedIn = req.session.loggedIn;
+
+        //Render the homepage view and pass the blog posts and loggedIn status
+        res.render('home/index', { blogPost, loggedIn });
+    } catch (err) {
+        // Handle any errors that occur
+        res.status(500).json(err);
+    }
+});
 
 module.exports = router;
