4bbbee45757c1e867e22d1c139c3bfab058600f8
PacoCasillas <pacocasillasmx@gmail.com>
2023-06-10 19:50:46 -0400

    fixed seed issues and seeded database successfully

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
+require("dotenv").config({ path: "../.env" });
 const Sequelize = require("sequelize");
-require("dotenv").config();
 
-// Create a new Sequelize instance with the databse credentials from the .env file
+// Create a new Sequelize instance with the database credentials from the .env file
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
--- seeds/seed.js
+++ seeds/seed.js
@@ -1,11 +1,19 @@
+const sequelize = require("../config/connection");
+const { User, BlogPost } = require("../models");
+const userData = require("./userData.json");
+const blogPostData = require("./blogPostData.json");
+
 async function seedDatabase() {
   await sequelize.sync({ force: true });
 
   // Seed users
-  await URLSearchParams.bulkCreate(userData, {
+  await User.bulkCreate(userData, {
     individualHooks: true,
     returning: true,
   });
+
+  // Seed blog posts
+  await BlogPost.bulkCreate(blogPostData);
 
   console.log("Database seeded successfully");
   process.exit(0);
